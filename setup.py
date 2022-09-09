be236eca622eaa64f4faf9b8f0851ed29d19d6fb
Nikos Argyropoulos <n.argyropoulos@oilx.co>
2022-09-09 16:31:34 +0300

    Changed setup.py to install dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,32 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
+# Check for dependencies
+install_requires = []
+try:
+    import resens
+except ImportError:
+    install_requires.append("resens>=0.4.2")
+try:
+    try:
+        import gdal
+    except ImportError:
+        from osgeo import gdal
+except ImportError:
+    install_requires.append("GDAL>=3.*")
+try:
+    import boto3
+except ImportError:
+    install_requires.append("boto3")
+try:
+    import tqdm
+except ImportError:
+    install_requires.append("tqdm")
+try:
+    import xmltodict
+except ImportError:
+    install_requires.append("xmltodict")
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
@@ -19,4 +45,5 @@ setup(
     package_dir={"": "s2awsdl"},
     python_requires=">=3.8",
     zip_safe=False,
+    install_requires=install_requires
 )
